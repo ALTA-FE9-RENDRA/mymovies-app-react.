a15098e9b5dc25dae2d4ec9a50e4177696ccd653
travelite <rendrabootcamp@gmail.com>
2022-10-05 02:55:41 +0700

    unfinished

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./pages";
 // import NowPlaying from "./pages/NowPlaying";
+// import DetailMovie from "./pages/DetailMovie";
+// import ListFavorite from "./pages/ListFavorite";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
